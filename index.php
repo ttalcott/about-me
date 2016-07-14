3b06fb53e03b3410997d2fa990ff539614b50593
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-14 16:56:18 -0600

    changed header font color

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 		<link href="css/style1.css" rel="stylesheet" type="text/css" />
 		<title>About Me</title>
 	</head>
-	<header>
-		<h1>About Me</h1>
-		<h3>A short web page all about Travis Talcott</h3>
-		<!--add picture here-->
-	</header>
 	<body>
+		<header>
+			<h1>About Me</h1>
+			<h3>A short web page all about Travis Talcott</h3>
+			<!--add picture here-->
+		</header>
 		<main>
 			<!--short paragraph about interests-->
 			<p class="red"> 
